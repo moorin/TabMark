b0355068884a4b514e2f2c7119ca35f4a6bdeb89
hyerin <onpa99@naver.com>
2020-09-02 21:59:47 +0900

    [get all tabs info] Proceeding

--- popup.js
+++ popup.js
@@ -9,17 +9,35 @@
 // index 0~len(tabs) 의 url을 가져와야한다.
 
 
-function save_btn()
+function save(btn_click)
 {
-  alert("test");
+  console.log("test");
+  btn_click.onclick=function()
+  {
+    console.log("clicked!");
+  };
 }
 
 window.onload = function()
 {
-  alert("1");
+  
   chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    //lastFocusedWindow: true 이기 때문에 tabs[0]은 마지막 탭
+    
     let url = tabs[0].url;
-    // use `url` here inside the callback because it's asynchronous!
+    let id = tabs[0].id;
+    let index = tabs[0].index;
+    let oldindex = index;
+    console.log(url,id,index);
+    
+    
+    chrome.tabs.goBack(); // 뒤로 이동
+    console.log(tabs[0].length);
   });
-  alert(url);
+  
+  
+  var btn_save = document.getElementsByClassName("popup_save")[0];
+  save(btn_save);
+  
+  
 };
